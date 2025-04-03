9ac1aecc24fb457324a7e1cc09d90c87c150de04
CaiGuoHui123 <118041591+CaiGuoHui123@users.noreply.github.com>
2025-04-03 14:48:44 +0800

    Add files via upload
    Upload project source code for ICME 2025

---
+++ go_train.py
@@ -0,0 +1,3 @@
+import os
+os.system("python train.py --data my_data.yaml --cfg yolov5s.yaml --weights pretrained/yolov5s.pt --epoch 400 --batch-size 8")
+
---
+++ uav_det.py
@@ -0,0 +1,3 @@
+import os
+
+os.system("python detect.py --weights runs/train/exp_transformer/weights/best.pt --source 0")
